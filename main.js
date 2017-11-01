10c92745ce230ef3a0ed848ee1ca65c50dc567ae
estherh5 <hoffman.esther@gmail.com>
2017-11-01 01:56:16 -0400

    Update styles to support different browsers
    - Add comments to code for clarity
    - Consolidate functions for simplicity
    - Consolidate image modals into single modal and switch out src/alt/title attributes when thumbnail is clicked for simplicity
    - Add keyboard shortcut to close modal when escape key is clicked
    - Replace navigation button tags with anchor tags for greater usability

--- main.js
+++ main.js
@@ -1,50 +1,72 @@
-// Define variables
+// Define global variables
 var header = document.getElementById('header');
 var images = document.getElementsByClassName('family-image');
-var modals = document.getElementsByClassName('modal');
-var zoomed = document.getElementsByClassName('zoomed');
-var closeButtons = document.getElementsByClassName('close');
-var supportsPassive = false;
+var modal = document.getElementById('modal');
+var modalImage = document.getElementById('modal-image');
+var closeButton = document.getElementById('close');
+var supportsPassive = false; // If browser supports passive event listeners
 
-// Define events
+
+// Assess if browser supports passive event listeners for faster processing
 try {
-  var opts = Object.defineProperty({}, 'passive', {
+  var options = Object.defineProperty({}, 'passive', {
     get: function() {
       supportsPassive = true;
+      return;
     }
   });
-  window.addEventListener('test', null, opts);
+  window.addEventListener('test', null, options);
 } catch (e) {}
 
+
+// Toggle shrink style on header when user scrolls
 window.addEventListener('scroll', function() {
   if (window.pageYOffset > 100) {
     header.classList.add('shrink');
+    return;
   } else if (header.classList.contains('shrink')) {
     header.classList.remove('shrink');
+    return;
   }
-}, supportsPassive ? { passive: true } : false)
+  return;
+}, supportsPassive ? { passive: true } : false);
 
+
+// Display zoomed-in image when user clicks thumbnail
 for (var i = 0; i < images.length; i++) {
-  images[i].addEventListener('click', modalDisplay, false);
+  images[i].addEventListener('click', function() {
+    modalImage.classList.remove('horizontal-zoomed');
+    modalImage.classList.remove('vertical-zoomed');
+    modalImage.classList.add(this.dataset.orientation + '-zoomed');
+    modalImage.src = this.src;
+    modalImage.title = this.title;
+    modalImage.alt = this.alt;
+    modal.classList.add('modal-display');
+    return;
+  }, false);
 }
 
-for (var i = 0; i < modals.length; i++) {
-  modals[i].addEventListener('click', modalHide, false);
-}
 
-for (var i = 0; i < closeButtons.length; i++) {
-  closeButtons[i].addEventListener('click', modalHide, false);
-}
-
-// Define functions
-function modalDisplay() {
-  modals[this.dataset.number].classList.add('modal-display');
-}
-
-function modalHide(e) {
-  if (e.target == this & this.classList.contains('modal-display')) {
-    this.classList.remove('modal-display');
-  } else if (this.classList.contains('close')) {
-    this.parentNode.classList.remove('modal-display');
+// Hide zoomed-in image when user clicks outside of image in modal,
+// clicks close button, or clicks escape key when modal is displayed
+modal.onclick = function(e) {
+  if (e.target == modal) {
+    modal.classList.remove('modal-display');
+    return;
   }
+  return;
+}
+
+closeButton.onclick = function() {
+  modal.classList.remove('modal-display');
+  return;
+}
+
+window.onkeydown = function(e) {
+  if (modal.classList.contains('modal-display') && e.keyCode == 27) {
+    e.preventDefault();
+    modal.classList.remove('modal-display');
+    return;
+  }
+  return;
 }
